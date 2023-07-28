3c2165764968f25aef493f361ad79bfc417d0d35
Pieter <pieterbrandsen2@gmail.com>
2023-07-28 22:44:27 +0200

    Added logger.js rotation

--- controller/src/logger.js
+++ controller/src/logger.js
@@ -1,89 +1,45 @@
 import { createLogger, format, transports } from "winston";
+import 'winston-daily-rotate-file';
 
-const { simple, combine, timestamp, prettyPrint, colorize, errors } = format;
 
-const baseFormat = () =>
-  combine(
-    errors({ stack: true }), // <-- use errors format
-    colorize(),
-    timestamp(),
-    prettyPrint()
-  );
-
-const _mainLogger = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
-  ],
-});
-
-if (process.env.NODE_ENV !== "production") {
-  _mainLogger.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
+function createCustomLogger(type) {
+  return createLogger({
+    level: 'info',
+    format: format.combine(
+      format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      format.printf(info => `${info.timestamp} ${info.level}: ${info.message} ${info.stack || ''}`),
+      format.errors({ stack: true })
+    ),
+    transports: [
+      new transports.DailyRotateFile({
+        filename: `logs/${type}/application-%DATE%.log`,
+        auditFile: `logs/${type}/audit.json`,
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      })
+    ],
+  });
 }
 
-const _apiLogger = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [
-    new transports.File({ filename: "logs/api/error.log", level: "error" }),
-    new transports.File({ filename: "logs/api/combined.log" }),
-  ],
-});
+const mainLogger = createCustomLogger('main');
+const apiLogger = createCustomLogger('api');
+const graphiteLogger = createCustomLogger('graphite');
 
-if (process.env.NODE_ENV !== "production") {
-  _apiLogger.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
+if (process.env.NODE_ENV !== 'production') {
+  mainLogger.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
+  apiLogger.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
+  graphiteLogger.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
 }
 
-const _graphiteLogger = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [new transports.File({ filename: "logs/graphite.log" })],
-});
-
-if (process.env.NODE_ENV !== "production") {
-  _graphiteLogger.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
-}
-
-export const mainLogger = {
-  info(log) {
-    _mainLogger.info(log);
-  },
-  error(log) {
-    _mainLogger.error(log);
-  },
-};
-
-export const apiLogger = {
-  info(log) {
-    _apiLogger.info(log);
-  },
-  error(log) {
-    _apiLogger.error(log);
-  },
-  debug(log) {
-    _apiLogger.debug(log);
-  },
-};
-
-export const graphiteLogger = {
-  info(log) {
-    _graphiteLogger.info(log);
-  },
-  error(log) {
-    _graphiteLogger.error(log);
-  },
-};
+export { mainLogger, apiLogger, graphiteLogger };
--- getter/src/logger.js
+++ getter/src/logger.js
@@ -1,116 +1,50 @@
 import { createLogger, format, transports } from "winston";
+import 'winston-daily-rotate-file';
 
-const { simple, combine, timestamp, prettyPrint, colorize, errors } = format;
 
-const baseFormat = () =>
-  combine(
-    errors({ stack: true }), // <-- use errors format
-    colorize(),
-    timestamp(),
-    prettyPrint()
-  );
-
-const _apiLogger = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [
-    new transports.File({
-      filename: "logs/api/error.log",
-      level: "error",
-    }),
-    new transports.File({ filename: "logs/api/combined.log" }),
-  ],
-});
-
-if (process.env.NODE_ENV !== "production") {
-  _apiLogger.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
+function createCustomLogger(type) {
+  return createLogger({
+    level: 'info',
+    format: format.combine(
+      format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      format.printf(info => `${info.timestamp} ${info.level}: ${info.message} ${info.stack || ''}`),
+      format.errors({ stack: true })
+    ),
+    transports: [
+      new transports.DailyRotateFile({
+        filename: `logs/${type}/application-%DATE%.log`,
+        auditFile: `logs/${type}/audit.json`,
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      })
+    ],
+  });
 }
 
-const _dataRequestBroker = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
-  ],
-});
-if (process.env.NODE_ENV !== "production") {
-  _dataRequestBroker.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
+const mainLogger = createCustomLogger('main');
+const apiLogger = createCustomLogger('api');
+const dataRequestBroker = createCustomLogger('dataRequestBroker');
+const backlogLogger = createCustomLogger('backlog');
+
+if (process.env.NODE_ENV !== 'production') {
+  mainLogger.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
+  apiLogger.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
+  dataRequestBroker.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
+  backlogLogger.add(new transports.Console({
+    format: format.combine(
+    )
+  }));
 }
 
-const _mainLogger = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
-  ],
-});
-if (process.env.NODE_ENV !== "production") {
-  _mainLogger.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
-}
-
-const _backlogLogger = createLogger({
-  level: "info",
-  format: baseFormat(),
-  transports: [new transports.File({ filename: "logs/backlog.log" })],
-});
-if (process.env.NODE_ENV !== "production") {
-  _backlogLogger.add(
-    new transports.Console({
-      format: simple(),
-    })
-  );
-}
-
-export const apiLogger = {
-  info(log) {
-    _apiLogger.info(log);
-  },
-  error(log) {
-    _apiLogger.error(log);
-  },
-  debug(log) {
-    _apiLogger.debug(log);
-  },
-};
-export const dataRequestBroker = {
-  info(log) {
-    _dataRequestBroker.info(log);
-  },
-  error(log) {
-    _dataRequestBroker.error(log);
-  },
-  debug(log) {
-    _dataRequestBroker.debug(log);
-  },
-};
-
-export const mainLogger = {
-  info(log) {
-    _mainLogger.info(log);
-  },
-  error(log) {
-    _mainLogger.error(log);
-  },
-};
-export const backlogLogger = {
-  info(log) {
-    _backlogLogger.info(log);
-  },
-  error(log) {
-    _backlogLogger.error(log);
-  },
-};
+export { mainLogger, apiLogger, dataRequestBroker, backlogLogger };
