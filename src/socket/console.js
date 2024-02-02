8cf153805b5ca4eacedb3cf4b40f428eab74b467
Pieter <pieterbrandsen2@gmail.com>
2024-02-02 09:26:51 +0000

    Re-enabled socket

--- src/socket/console.js
+++ src/socket/console.js
@@ -17,12 +17,12 @@ if (process.env.PRIVATE_SERVER_USERNAME) {
 
   const url = `${process.env.PRIVATE_SERVER_PROTOCOL}://${process.env.PRIVATE_SERVER_HOST}:${process.env.PRIVATE_SERVER_PORT}`
 
-  // api.socket.connect()
+  api.socket.connect()
   api.socket.on('connected', () => {
     console.log('Connected to socket')
   })
 
-  // api.socket.subscribe('console')
+  api.socket.subscribe('console')
   api.socket.on('console', async (event) => {
     try {
       const { data } = await axios.get(`${url}/stats`);
